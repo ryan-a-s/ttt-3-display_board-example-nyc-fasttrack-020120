e3f343714228f84c3c03d6864ecfcf2a78285a9c
Ryan Seit <ryan@ryanseit.com>
2020-02-01 17:43:24 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,11 +2,11 @@
 
 def display_board
   puts "Welcome to Tic Tac Toe"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+  puts "   |   |   \n"
+  puts "-----------\n"
+  puts "   |   |   \n"
+  puts "-----------\n"
+  puts "   |   |   \n"
 end
 
 print display_board
